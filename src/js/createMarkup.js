d5eba2807a4efd498957b1762e9014491e97fff3
Yevhen <leshchenkoyevhen@gmail.com>
2022-10-16 14:31:09 -0400

    add some styles

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -2,19 +2,19 @@ export function createMarkup(images) {
   return images
     .map(({ largeImageURL, tags, likes, views, comments, downloads }) => {
       return `<div class="photo-card">
-      <img src="${largeImageURL}" alt="${tags}" loading="lazy" width="250" height="250" />
+      <img src="${largeImageURL}" alt="${tags}" loading="lazy" width="450" height="250" />
       <div class="info">
         <p class="info-item">
-          <b>Likes ${likes}</b>
+          <b>Likes: ${likes}</b>
         </p>
         <p class="info-item">
-          <b>Views ${views}</b>
+          <b>Views: ${views}</b>
         </p>
         <p class="info-item">
-          <b>Comments ${comments}</b>
+          <b>Comments: ${comments}</b>
         </p>
         <p class="info-item">
-          <b>Downloads ${downloads}</b>
+          <b>Downloads: ${downloads}</b>
         </p>
       </div>
     </div>`;
